c411980fce8da550b1e1ecfd4e1c3db2e3cae97f
nsdesai1712 <90010452+nsdesai1712@users.noreply.github.com>
2023-11-17 08:02:08 -0500

    updates

--- ebdjango/settings.py
+++ ebdjango/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-o+o2sjnfanxl52u(^q8tsd4&-3t#^js--4b-+6qb7sz##u2510
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["predict-service-virtualenv.eba-r3ddyqxb.us-east-2.elasticbeanstalk.com"]
 
 
 # Application definition
