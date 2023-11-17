7665d28c6988a72ea2661e860e8a40714aeec39a
nsdesai1712 <90010452+nsdesai1712@users.noreply.github.com>
2023-11-17 01:27:38 -0500

    updates

--- ebdjango/urls.py
+++ ebdjango/urls.py
@@ -1,4 +1,4 @@
-"""chest_cancer_detection URL Configuration
+"""ebdjango URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
--- run.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d %~dp0
-python manage.py runserver
-start
-http://127.0.0.1:8000/home_async
